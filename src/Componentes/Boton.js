af855a34fbc931b30047de99426cf7431863b619
GeorginaTL <georginatelloleon@gmail.com>
2024-10-23 21:21:08 -0700

    estructura numeros

--- src/Componentes/Boton.js
+++ src/Componentes/Boton.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import '../hojas-de-estilo/Boton.css'
 
 function Boton(props){
+  const esOperador=valor=>{
+    return isNaN(valor)&&(valor != '.')&&(valor != '=');
+  };
   return(
-    <div>
+    <div className={`boton-contenedor ${esOperador(props.children)?'operador':''}`.trimEnd()}>
       {props.children}
     </div>
   );

c6dbfcfec93a9decc72f81398a8d9407183cfbbd
GeorginaTL <georginatelloleon@gmail.com>
2024-10-23 20:09:21 -0700

    logo centrado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import freeCodeCamoLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
-      Hola
+      <div className='freecodecamp-logo-contenedor'>
+      <img 
+          className='freecodecamp-logo'
+          alt='Logo de Free Code Camp'
+          src={freeCodeCamoLogo}/>
+      </div>
     </div>
   );
 }
